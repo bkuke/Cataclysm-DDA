fbb3351ba9cceeb0712305ea7a52edd957e9d383
bkuke <33252982+bkuke@users.noreply.github.com>
2021-02-15 16:56:33 +0800

    json误我

--- android/app/src/main/java/com/cleverraven/cataclysmdda/ModList.java
+++ android/app/src/main/java/com/cleverraven/cataclysmdda/ModList.java
@@ -5,12 +5,10 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import java.io.File;
 
 public class ModList extends AppCompatActivity {
-    private static final String TAG = "CDDA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +17,13 @@ public class ModList extends AppCompatActivity {
         File[] externalStorageVolumes = ContextCompat.getExternalFilesDirs(getApplicationContext(), null);
         File primaryExternalStorage = externalStorageVolumes[0];
         int keycode=this.getIntent().getIntExtra("key",0);
-        String key=null;
+        String key;
         if (keycode == 1) {
             key = "Mod.json";
         } else {
             key = "SoundPack.json";
         }
-        String fp = primaryExternalStorage.getAbsolutePath() + "/data/json/"+key;
+        String fp = primaryExternalStorage.getAbsolutePath() + "/data/"+key;
         RecyclerView recyclerView=findViewById(R.id.mdl);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         RecyclerViewAdapter adapter=new RecyclerViewAdapter(this,fp,keycode);
--- android/app/src/main/java/com/cleverraven/cataclysmdda/WelcomScreen.java
+++ android/app/src/main/java/com/cleverraven/cataclysmdda/WelcomScreen.java
@@ -3,18 +3,15 @@ package com.cleverraven.cataclysmdda;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class WelcomScreen extends AppCompatActivity implements View.OnClickListener {
-
-    private static final String TAG = "CDDA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
